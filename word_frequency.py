d7328c2538849e95b8a2ff56a7866e148bf5549a
BelongaRobert <68036456+BelongaRobert@users.noreply.github.com>
2020-08-13 09:52:03 -0400

    Too many Words

--- word_frequency.py
+++ word_frequency.py
@@ -5,23 +5,52 @@ STOP_WORDS = [
 ]
 
 
-def print_word_freq(file):
-    """Read in `file` and print out the frequency of words in that file."""
-    pass
+def print_word_freq(fname):
+    counts = {}
+
+    with open(fname) as infile:
+        words = infile.read().lower().split()
+
+    for word in words:
+        filtered = []
+        
+        for c in word:
+            if c.isalpha():
+                filtered.append(c)
+
+        new = "".join(filtered)
+
+        if new not in STOP_WORDS:
+            if new in counts:
+                counts[new] = counts[new] + 1
+            
+            else:
+                counts[new] = 1
+
+    width = max([len(k) for k in counts])
+    for k in counts:
+        print(f"{k:>{width}} | {counts[k]} {'*' * counts[k]}")
 
 
 if __name__ == "__main__":
-    import argparse
-    from pathlib import Path
+    import sys
 
-    parser = argparse.ArgumentParser(
-        description='Get the word frequency in a text file.')
-    parser.add_argument('file', help='file to read')
-    args = parser.parse_args()
+    try:
+        filename = sys.argv[1]
+        
+        if "-v" in sys.argv:
+            print(f"This is the value of sys.argv: {sys.argv}")
+            print(f"This is the filename you gave as an argument: {filename}")
 
-    file = Path(args.file)
-    if file.is_file():
-        print_word_freq(file)
-    else:
-        print(f"{file} does not exist!")
+        print_word_freq(filename)
+
+    except IndexError:
+        print("usage: python3 word_frequency.py <filename>")
         exit(1)
+
+    except FileNotFoundError:
+        print(f"No file named {filename} exists!")
+        exit(1)
+
+    else:
+        exit(0)
